995265fee7fe8496cd20b4162fa20e58fe2c4c07
Guilherme Luiz <guilhermelpp.eng@gmail.com>
2021-09-17 09:20:51 -0300

    Adição objeto plano

--- src/Dominio/Fabrica/VetorFabrica.php
+++ src/Dominio/Fabrica/VetorFabrica.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Solidbase\Geometria\Dominio\Fabrica;
 
+use DomainException;
 use Solidbase\Geometria\Dominio\Ponto;
 use Solidbase\Geometria\Dominio\Vetor;
 
@@ -34,5 +35,18 @@ final class VetorFabrica
     public static function BaseZ(): Vetor
     {
         return new Vetor(0, 0, 1);
+    }
+
+    public static function Perpendicular(Vetor $vetor): Vetor
+    {
+        if ($vetor->eNulo()) {
+            throw new DomainException('Não é possível gerar vetor perpendiculares a partir de vetores nulos');
+        }
+        $baseZ = self::BaseZ();
+        if ($vetor->temMesmaDirecao($baseZ)) {
+            return self::BaseX();
+        }
+
+        return $vetor->produtoVetorial($baseZ);
     }
 }
--- src/Dominio/Plano.php
+++ src/Dominio/Plano.php
@@ -4,7 +4,37 @@ declare(strict_types=1);
 
 namespace Solidbase\Geometria\Dominio;
 
+use DomainException;
+use Solidbase\Geometria\Dominio\Fabrica\VetorFabrica;
+
 class Plano
 {
-    
+    public const PRECISAO = 1E-10;
+
+    private Vetor $u;
+    private Vetor $v;
+
+    public function __construct(
+        protected Ponto $origem,
+        protected Vetor $normal
+    ) {
+        if ($normal->eNulo()) {
+            throw new DomainException('Não é possível criar Planos com normal nula');
+        }
+        $this->normal = $normal->vetorUnitario();
+        $this->u = VetorFabrica::Perpendicular($normal)->vetorUnitario();
+        $this->v = $this->u->produtoVetorial($this->normal)->vetorUnitario();
+    }
+
+    public function distanciaPontoAoPlano(Ponto $ponto): float
+    {
+        $v = VetorFabrica::apartirDoisPonto($this->origem, $ponto);
+
+        return $this->normal->produtoInterno($v);
+    }
+
+    public function pontoPertenceAoPlano(Ponto $ponto): bool
+    {
+        return abs($this->distanciaPontoAoPlano($ponto)) <= $this::PRECISAO;
+    }
 }
