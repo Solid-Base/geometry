37ac8a5ab55aa885903a30007345e6231aa32a90
Guilherme Luiz <guilhermelpp.eng@gmail.com>
2022-01-25 13:46:52 -0300

    Intersecção linha com polilinha

--- src/Aplicacao/Interseccao/IntersecaoLinhaPolilinha.php
+++ src/Aplicacao/Interseccao/IntersecaoLinhaPolilinha.php
@@ -73,4 +73,60 @@ class IntersecaoLinhaPolilinha
 
         return $this->poligonoNovo;
     }
+
+    public static function Pontos(Linha $linha, Polilinha $poligono): ?array
+    {
+        [$pontos,] = self::executarIntersecao($linha, $poligono);
+        if (0 == count($pontos)) {
+            return null;
+        }
+
+        return $pontos;
+    }
+
+    public static function Poligono(Linha $linha, Polilinha $poligono): Polilinha
+    {
+        [,$poligono] = self::executarIntersecao($linha, $poligono);
+
+        return $poligono;
+    }
+
+    private static function executarIntersecao(Linha $linhaIntersecao, Polilinha $poligono): array
+    {
+        $polilinha = clone $poligono;
+        $polilinha->fecharPolilinha();
+        $pontos = $polilinha->pontos();
+        $numeroPonto = \count($pontos);
+        $pontosRetorno = [];
+        $pontosPoligono = [];
+        for ($i = 1; $i < $numeroPonto; ++$i) {
+            $p1 = $pontos[$i - 1];
+            $p2 = $pontos[$i];
+            $linha = LinhaFabrica::apartirDoisPonto($p1, $p2);
+            if ($linha->eParelo($linhaIntersecao)) {
+                $pontosPoligono[] = $p2;
+
+                continue;
+            }
+            $intersecao = new InterseccaoLinhas($linhaIntersecao, $linha);
+            $ponto = $intersecao->executar();
+            if (false !== array_search($ponto, $pontosRetorno, false)) {
+                $pontosPoligono[] = $p2;
+
+                continue;
+            }
+            if ($linha->pontoPertenceSegmento($ponto)) {
+                $pontosPoligono[] = $ponto;
+                $pontosPoligono[] = $p2;
+                $pontosRetorno[] = $ponto;
+
+                continue;
+            }
+
+            $pontosPoligono[] = $p2;
+        }
+        $poligonoNovo = PolilinhaFabrica::criarPolilinhaPontos($pontosPoligono);
+
+        return [$pontosPoligono, $poligonoNovo];
+    }
 }
