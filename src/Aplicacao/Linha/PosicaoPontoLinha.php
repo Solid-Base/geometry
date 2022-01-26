2bd202c948d8a80e1218f39f1e17926165686651
Guilherme Luiz <guilhermelpp.eng@gmail.com>
2022-01-26 13:22:04 -0300

    Adicionado posição de ponto relativa a reta

---
+++ src/Aplicacao/Linha/PosicaoPontoLinha.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Solidbase\Geometria\Aplicacao\Linha;
+
+use Solidbase\Geometria\Dominio\Fabrica\PlanoFabrica;
+use Solidbase\Geometria\Dominio\Fabrica\VetorFabrica;
+use Solidbase\Geometria\Dominio\Linha;
+use Solidbase\Geometria\Dominio\Ponto;
+
+class PosicaoPontoLinha
+{
+    public static function executar(Linha $linha, Ponto $ponto): PosicaoPontoEnum
+    {
+        if ($linha->pontoPertenceLinha($ponto)) {
+            return PosicaoPontoEnum::SOBRE;
+        }
+        $plano = PlanoFabrica::criarPlanoOrigemBases(new Ponto(), VetorFabrica::BaseX(), VetorFabrica::BaseY());
+
+        $origem = $linha->origem;
+        $direcao = $linha->direcao;
+        $vetor1 = VetorFabrica::apartirDoisPonto($origem, $ponto);
+        $resultado = $direcao->produtoVetorial($vetor1);
+        if (eMaior($resultado->z, 0)) {
+            return PosicaoPontoEnum::ESQUERDA;
+        }
+
+        return PosicaoPontoEnum::DIREITA;
+    }
+}
