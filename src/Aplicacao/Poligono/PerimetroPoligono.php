abf9b2d819268fdb9853673d0af07c24fc9839e0
Guilherme Luiz <guilhermelpp.eng@gmail.com>
2022-02-02 14:34:17 -0300

    Adição do calculo de perimetro de um poligono

---
+++ src/Aplicacao/Poligono/PerimetroPoligono.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Solidbase\Geometria\Aplicacao\Poligono;
+
+use Solidbase\Geometria\Dominio\Polilinha;
+
+class PerimetroPoligono
+{
+    private function __construct()
+    {
+    }
+
+    public static function executar(Polilinha $poligono): ?float
+    {
+        $soma = 0;
+        if (\count($poligono) < 3) {
+            return null;
+        }
+        $poligono->fecharPolilinha();
+        $pontos = $poligono->pontos();
+        $quantidade = count($pontos);
+        for ($i = 1; $i < $quantidade; ++$i) {
+            $p1 = $pontos[$i - 1];
+            $p2 = $pontos[$i];
+            $soma += $p1->distanciaParaPonto($p2);
+        }
+
+        return $soma;
+    }
+}
